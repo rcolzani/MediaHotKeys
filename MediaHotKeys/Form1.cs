e6c278edf762ff7d492bbb43a9aa00da1eb722da
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2020-11-25 20:21:50 -0300

    Add project files.

---
+++ MediaHotKeys/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaHotKeys
+{
+    public partial class Form1 : Form
+    {
+        private MediaHotKeys.GlobalHandle PvGlobalMediaHotkeys;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PvGlobalMediaHotkeys = new MediaHotKeys.GlobalHandle(GlobalHandle.Constants.ALT + GlobalHandle.Constants.SHIFT, Keys.Right,  this);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        /// <summary>
+        /// Método que recebe as mensagens que o Windows dispara. 
+        /// Como este método não recebe apenas os comandos que nossa aplicação registrou, é necessário validar se o comando recebido é o mesmo que foi registrado.
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == MediaHotKeys.GlobalHandle.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// Função que será executada a partir do atalho de teclas pressionado
+        /// </summary>
+        private void HandleHotkey()
+        {
+             Invoke(new MethodInvoker(() => SendMessage(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)((int)14 << 16))));
+        }
+
+        private const int WM_APPCOMMAND = 0x319;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!PvGlobalMediaHotkeys.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             Console.WriteLine("Trying to register SHIFT+ALT+O");
+            if (PvGlobalMediaHotkeys.Register())
+                Console.WriteLine("Hotkey registered.");
+            else
+                Console.WriteLine("Hotkey failed to register");
+
+        }
+    }
+}
